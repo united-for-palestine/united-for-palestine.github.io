4aa7c5c52633c8ab4e38f08c395d733985919d94
bas081 <bas080@hotmail.com>
2024-10-10 09:40:25 +0300

    fixup! Start 11ty project rewrite

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,5 +1,6 @@
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import markdownIt from "markdown-it";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(feedPlugin, {
@@ -28,13 +29,9 @@ export default function (eleventyConfig) {
   ];
 
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-    // which file extensions to process
     extensions: "html",
-    // optional, output image formats
     formats: ["jpg", "webp"],
-    // optional, output image widths
     widths: ["auto", 400, 800],
-    // optional, attributes assigned on <img> override these values.
     defaultAttributes: {
       loading: "lazy",
       sizes: "100vw",
@@ -44,22 +41,41 @@ export default function (eleventyConfig) {
 
   eleventyConfig.addFilter("breadcrumb", function (currentUrl) {
     return breadcrumbNamesArray.filter(({ url }) => {
-      return currentUrl.startsWith(url);
+      return currentUrl.startsWith(url) && currentUrl !== url;
     });
   });
 
-  eleventyConfig.addFilter('date', function(date) {
-    if (!date) return '';
+  eleventyConfig.addFilter("date", function (date) {
+    if (!date) return "";
 
     const options = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
+      year: "numeric",
+      month: "long",
+      day: "numeric",
     };
 
     return new Date(date).toLocaleString(this.lang, options);
   });
 
+  const md = markdownIt({
+    html: true,
+    linkify: true,
+  });
 
+  md.renderer.rules.image = (tokens, idx, options, env, self) => {
+    const token = tokens[idx];
+    const src = token.attrGet("src");
+    const alt = token.content;
 
+    const caption = md.renderInline(alt, env);
+
+    return `
+        <figure>
+          <img src="${src}" alt="${alt}" />
+          <figcaption>${caption}</figcaption>
+        </figure>
+      `;
+  };
+
+  eleventyConfig.setLibrary("md", md);
 }
